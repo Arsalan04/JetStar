6234d2df0e87119a3131da827be4fd818838bd14
Arsalan <arsalancardoz04@gmail.com>
2022-06-29 14:08:55 +0530

    Added close functionality on special offer div

---
+++ app.js
@@ -0,0 +1,4 @@
+// Close Special Offer div
+const offerDiv = document.querySelector('.special-offer');
+const closeBtn = document.querySelector('.special-offer');
+closeBtn.addEventListener('click', () => offerDiv.style.display = "none")
